39f64fa1f149e57d0922b374782fbbc52cec7f8a
didar <deddarnawzad@gmail.com>
2025-03-08 15:12:56 +0300

    main : delete button and anchor in home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,4 @@
 	import { localizeHref, setLocale } from '../paraglide/runtime';
 </script>
 
-<button class="border-x px-2 py-1" onclick={() => setLocale('ckb')}>ckb</button>
-<button class="border-x px-2 py-1" onclick={() => setLocale('ar')}>ar</button>
-<button class="border-x px-2 py-1" onclick={() => setLocale('en')}>en</button>
-<br/>
-<a class="border-x px-2 py-1" href={localizeHref('/about')}>about</a>
-<a class="border-x px-2 py-1" href={localizeHref('/policy')}>policy</a>
-<a class="border-x px-2 py-1" href={localizeHref('/123')}>postId</a>
 <p>{m.greeting()}</p> 
