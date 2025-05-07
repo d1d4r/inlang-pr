9d8536f39c27a355097117a7b1297122096e4563
didar <deddarnawzad@gmail.com>
2025-05-07 21:59:16 +0300

    main : ....

--- src/routes/(main)/+page.ts
+++ src/routes/(main)/+page.ts
@@ -1,9 +1,7 @@
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
-
-
-    return {
-        random : Math.random()
-    }
+	return {
+		random: Math.random()
+	};
 }) satisfies PageLoad;
