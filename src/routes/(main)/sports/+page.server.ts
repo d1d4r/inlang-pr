65b587ea329a52ff86d020d81879e220037f27af
didar <deddarnawzad@gmail.com>
2025-06-24 21:27:24 +0300

    main : delete page server sport and prerender

--- src/routes/(main)/sports/+page.server.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-	isr: {
-		expiration: false,
-		bypassToken: 'b14c35e3-5670-4eaa-8d31-377e1f6dbf99'
-	}
-};
--- src/routes/(main)/sports/+page.ts
+++ src/routes/(main)/sports/+page.ts
@@ -1,5 +1,5 @@
 import type { PageLoad } from './$types';
-
+export const prerender = true;
 export const load = (async () => {
     return {
         random: Math.random()
