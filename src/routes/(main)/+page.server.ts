5ab6635332f679792940f9a75b30be5e2cb4bd8c
d1d4r <deddarnawzad@gmail.com>
2025-12-28 00:08:41 +0300

    main : remove runtime

--- src/routes/(main)/+page.server.ts
+++ src/routes/(main)/+page.server.ts
@@ -1,6 +1,5 @@
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 export const config = {
-	runtime: 'nodejs', // Required for ISR
 	isr: {
 		expiration: false,
 		bypassToken: 'didardidardidardidardidardidardidar'
